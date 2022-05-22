b1d01460d691a10ac597ddca10a982d0fd7f0e9b
denis <permwed@gmail.com>
2022-05-22 17:37:17 +0500

    connected to mongo

--- api/index.js
+++ api/index.js
@@ -1,1 +1,27 @@
-console.log('Start!');
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MONGO DB");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("MondoDB disconnected");
+});
+mongoose.connection.on("connected", () => {
+  console.log("MondoDB connected");
+});
+
+app.listen(8800, () => {
+  connect();
+  console.log("Work!!!");
+});

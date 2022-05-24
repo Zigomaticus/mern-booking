277c65cb7f16b0c557849850f68f579cd0ce6356
denis <permwed@gmail.com>
2022-05-24 22:24:57 +0500

    end a first part of backend

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,7 +1,28 @@
 import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getAllRooms,
+  getRoom,
+  updateRoom,
+} from "../controllers/roomController.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+// Create
+router.post("/:hotelId", verifyAdmin, createRoom);
 
+// Update
+router.put("/:id", verifyAdmin, updateRoom);
+
+// Delete
+router.delete("/:id/:hotelId", verifyAdmin, deleteRoom);
+
+// Get one
+router.get("/:id", getRoom);
+
+// Get all
+router.get("/", getAllRooms);
 
 export default router;

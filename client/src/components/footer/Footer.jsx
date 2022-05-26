076b0372942e7dc2f6aa550b30660350f3c7c21e
denis <permwed@gmail.com>
2022-05-26 11:16:11 +0500

    add footer

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+// Css
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="fLists">
+        <ul className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Regions</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Discricts</li>
+          <li className="fListItem">Airports</li>
+          <li className="fListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Regions</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Discricts</li>
+          <li className="fListItem">Airports</li>
+          <li className="fListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Regions</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Discricts</li>
+          <li className="fListItem">Airports</li>
+          <li className="fListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Regions</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Discricts</li>
+          <li className="fListItem">Airports</li>
+          <li className="fListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Regions</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Discricts</li>
+          <li className="fListItem">Airports</li>
+          <li className="fListItem">Hotels</li>
+        </ul>
+      </div>
+      <div className="fText">Copyright @ 2022 MERN Booking</div>
+    </div>
+  );
+};
+
+export default Footer;

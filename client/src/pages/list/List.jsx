865f0dd874fa55a4a35b14c181a3695ae291bfad
denis <permwed@gmail.com>
2022-05-26 11:44:52 +0500

    working on the hotels

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -2,12 +2,22 @@ import React from "react";
 // Components
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
+// Css
+import "./List.css";
 
 const List = () => {
   return (
     <div>
       <Navbar />
       <Header type="list" />
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="lsTitle">Search</h1>
+          </div>
+          <div className="listResult"></div>
+        </div>
+      </div>
     </div>
   );
 };
